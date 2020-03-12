0f1d259d63f24d827a61e916c485a291123c1293
prakashebi <prakash@ebi.ac.uk>
2020-03-12 11:50:59 +0000

    build code

---
+++ precache-manifest.f0922a58b1cb547c7974de4222923db7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae182dc2dcd675d9964cbc7f3ef0f237",
+    "url": "/dev-competency-mapper/index.html"
+  },
+  {
+    "revision": "9f0d47bd3e8b3509c8fe",
+    "url": "/dev-competency-mapper/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "6aa7521451ab42211ee4",
+    "url": "/dev-competency-mapper/static/css/main.0ed8c25c.chunk.css"
+  },
+  {
+    "revision": "9f0d47bd3e8b3509c8fe",
+    "url": "/dev-competency-mapper/static/js/2.3dabb6e3.chunk.js"
+  },
+  {
+    "revision": "6aa7521451ab42211ee4",
+    "url": "/dev-competency-mapper/static/js/main.70e2241f.chunk.js"
+  },
+  {
+    "revision": "6637875d2855b9cafbf2",
+    "url": "/dev-competency-mapper/static/js/runtime~main.5713b721.js"
+  }
+]);

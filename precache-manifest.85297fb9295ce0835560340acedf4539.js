1d89bc8e1b542ff0660e5eb3988b0053b304b17c
prakash <prakash@ebi.ac.uk>
2020-10-21 10:25:58 +0530

    consolidated look and feel

--- precache-manifest.85297fb9295ce0835560340acedf4539.js
+++ precache-manifest.85297fb9295ce0835560340acedf4539.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "06b685426e3a9e15970bce39449c2e73",
+    "revision": "283f762ea562662c221bf26843ab2833",
     "url": "/dev-competency-mapper/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/dev-competency-mapper/static/css/2.8f0705b9.chunk.css"
   },
   {
-    "revision": "fac60ad81c86d8e0956b",
-    "url": "/dev-competency-mapper/static/css/main.ee26a7f7.chunk.css"
+    "revision": "9685f4c364e46696bf57",
+    "url": "/dev-competency-mapper/static/css/main.4d083bfe.chunk.css"
   },
   {
     "revision": "8410661ffd75f68ab210",
     "url": "/dev-competency-mapper/static/js/2.70c9dd94.chunk.js"
   },
   {
-    "revision": "fac60ad81c86d8e0956b",
-    "url": "/dev-competency-mapper/static/js/main.715c740d.chunk.js"
+    "revision": "9685f4c364e46696bf57",
+    "url": "/dev-competency-mapper/static/js/main.76445410.chunk.js"
   },
   {
     "revision": "6637875d2855b9cafbf2",
